25a2b4884829f213dd19f1926d50be0142c0f8f3
bijoy <bijoysingh693@gmail.com>
2017-11-01 21:58:14 +0000

    Fixing Bug

--- app/src/main/java/com/bijoysingh/quicknote/database/AppDatabase.java
+++ app/src/main/java/com/bijoysingh/quicknote/database/AppDatabase.java
@@ -1,13 +1,15 @@
 package com.bijoysingh.quicknote.database;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.migration.Migration;
 import android.content.Context;
 
 @Database(
     entities = {Note.class},
-    version = 1
+    version = 2
 )
 public abstract class AppDatabase extends RoomDatabase {
 
@@ -20,8 +22,16 @@ public abstract class AppDatabase extends RoomDatabase {
       database = Room
           .databaseBuilder(context, AppDatabase.class, "note-database")
           .allowMainThreadQueries()
+          .addMigrations(MIGRATION_1_2)
           .build();
     }
     return database;
   }
+
+  static final Migration MIGRATION_1_2 = new Migration(1, 2) {
+    @Override
+    public void migrate(SupportSQLiteDatabase database) {
+
+    }
+  };
 }
