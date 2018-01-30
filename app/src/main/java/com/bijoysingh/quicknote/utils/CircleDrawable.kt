83a93ae433719cafb0778e7545d3bba3db8e854b
Bijoy Singh Kochar <bijoysingh693@gmail.com>
2018-01-30 22:40:49 +0000

    Adding border to the color circles

--- app/src/main/java/com/bijoysingh/quicknote/utils/CircleDrawable.kt
+++ app/src/main/java/com/bijoysingh/quicknote/utils/CircleDrawable.kt
@@ -1,24 +1,26 @@
 package com.bijoysingh.quicknote.utils
 
-import android.graphics.Canvas
-import android.graphics.ColorFilter
-import android.graphics.Paint
-import android.graphics.PixelFormat
-import android.graphics.Rect
+import android.graphics.*
 import android.graphics.drawable.Drawable
 
 class CircleDrawable(color: Int) : Drawable() {
   private val paint: Paint
+  private val borderPaint: Paint
   private var radius = 0
 
   init {
     this.paint = Paint(Paint.ANTI_ALIAS_FLAG)
     this.paint.color = color
+
+    val isNightTheme = ThemeManager.themeManager?.isNightTheme() ?: false
+    this.borderPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    this.borderPaint.color = if(isNightTheme) Color.LTGRAY else Color.GRAY
   }
 
   override fun draw(canvas: Canvas) {
     val bounds = bounds
-    canvas.drawCircle(bounds.centerX().toFloat(), bounds.centerY().toFloat(), radius.toFloat(), paint)
+    canvas.drawCircle(bounds.centerX().toFloat(), bounds.centerY().toFloat(), radius.toFloat(), borderPaint)
+    canvas.drawCircle(bounds.centerX().toFloat(), bounds.centerY().toFloat(), radius.toFloat() - 2, paint)
   }
 
   override fun setAlpha(alpha: Int) {
