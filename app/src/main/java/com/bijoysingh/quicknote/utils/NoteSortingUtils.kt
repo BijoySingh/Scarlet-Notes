41d63b5ecafa2611c38befee2d3e231964618ce7
Bijoy Singh Kochar <bijoysingh693@gmail.com>
2018-03-17 17:49:53 +0000

    String updates and launching v4.7

--- app/src/main/java/com/bijoysingh/quicknote/utils/NoteSortingUtils.kt
+++ app/src/main/java/com/bijoysingh/quicknote/utils/NoteSortingUtils.kt
@@ -5,10 +5,6 @@ import com.bijoysingh.quicknote.database.Note
 
 fun sort(notes: List<Note>, sortingTechnique: SortingTechnique): List<Note> {
   // Notes returned from DB are always sorted newest first. Reduce computational load
-  if (sortingTechnique == SortingTechnique.NEWEST_FIRST) {
-    return notes
-  }
-
   return when (sortingTechnique) {
     SortingTechnique.LAST_MODIFIED -> notes.sortedByDescending { note -> if (note.pinned) Long.MAX_VALUE else note.updateTimestamp }
     SortingTechnique.OLDEST_FIRST -> notes.sortedBy { note -> if (note.pinned) Long.MIN_VALUE else note.timestamp }
